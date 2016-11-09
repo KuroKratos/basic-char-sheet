3f46b63e2ae76b5aa2057233a1894bc827ab6e1f
jboisson <j.boisson@thk.eu>
2016-11-09 11:34:04 +0100

    Allow to roll without using mod

--- roll.php
+++ roll.php
@@ -2,5 +2,5 @@
 
 if(!empty($_POST['roll'])) {
   include_once './dice.class.php';
-  Dice::roll($_POST['roll'], $_POST['char'], $_POST['mod'], $_POST['type']);
+  Dice::roll($_POST['roll'], $_POST['char'], $_POST['mod'], $_POST['type'], $_POST['disp_mod']);
 }
