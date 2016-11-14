4ad83517de753d445423b1d1d52543a7fac57c7f
jboisson <j.boisson@thk.eu>
2016-11-14 15:22:16 +0100

    Changed classes dependences and added User class

--- classes/character.class.php
+++ classes/character.class.php
@@ -1,40 +1,47 @@
 <?php
-  class character {
+class character {
 
-    public static function get ($data) {
+  public static function get ($data) {
 
-      $return = [];
+    $return = [];
 
-      $sql = "SELECT
-                char_name name,
-                char_level level,
-                char_current_exp exp,
-                char_max_exp max_exp,
-                char_current_hp hp,
-                char_max_hp max_hp,
-                char_current_mp mp,
-                char_max_mp max_mp,
-                char_gold gold,
-                char_str_mod `str`,
-                char_psy_mod psy,
-                char_luk_mod luk,
-                char_def_mod def,
-                (CASE WHEN (char_sex = 0) THEN cl.class_name_m ELSE cl.class_name_f END) as class,
-                (CASE WHEN (char_sex = 0) THEN r.race_name_m ELSE r.race_name_f END) as race
-              FROM `character` ch
-              INNER JOIN class cl ON ch.char_class = cl.class_id
-              INNER JOIN race r ON ch.char_race = r.race_id
-              WHERE char_name = '".$data['name']."'";
+    $sql = "SELECT
+              char_name name,
+              char_level level,
+              char_current_exp exp,
+              char_max_exp max_exp,
+              char_current_hp hp,
+              char_max_hp max_hp,
+              char_current_mp mp,
+              char_max_mp max_mp,
+              char_gold gold,
+              char_str_mod `str`,
+              char_psy_mod psy,
+              char_luk_mod luk,
+              char_def_mod def,
+              (CASE WHEN (char_sex = 0) THEN cl.class_name_m ELSE cl.class_name_f END) as class,
+              (CASE WHEN (char_sex = 0) THEN r.race_name_m ELSE r.race_name_f END) as race
+            FROM `character` ch
+            INNER JOIN class cl ON ch.char_class = cl.class_id
+            INNER JOIN race r ON ch.char_race = r.race_id
+            WHERE char_name = '".$data['name']."'";
 
-      $stmt = DB::run($sql);
-      while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-      {
-        foreach($row as $key => $val) {
-          $return[$key] = $val;
-        }
+    $stmt = DB::run($sql);
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+    {
+      foreach($row as $key => $val) {
+        $return[$key] = $val;
       }
-
-      return $return;
     }
+
+    return $return;
   }
+
+  public static function getList($table = 'character') {
+    $sql = "SELECT * FROM $table";
+    $stmt = DB::run($sql);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+}
 ?>
--- classes/dice.class.php
+++ classes/dice.class.php
@@ -1,8 +1,4 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 class Dice {
 
   public static function roll($diceString, $name = "", $mod = 0, $type = "", $disp_mod = false) {
