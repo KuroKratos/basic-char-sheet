c3f81d99da51175c7b6100376734f5db51d89da6
jboisson <j.boisson@thk.eu>
2016-11-22 15:51:02 +0100

    Commit & Push test for GitLab

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);
+
+  //error_reporting(E_ALL);
+  //ini_set('display_errors', 1);
 
   require_once('./classes/db.class.php');
   require_once('./classes/character.class.php');
