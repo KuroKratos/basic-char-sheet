2662f234df509203be5069eb06ec28c4d0f60c15
jboisson <j.boisson@thk.eu>
2017-01-05 10:46:04 +0100

    Roll functions almost finished

--- roller.php
+++ roller.php
@@ -50,6 +50,18 @@
             <a name="roll" id="100" class="btn btn-default">D100</a>
           </div>
 
+          <div class="col-sm-12" style="margin-bottom: 10px;">
+            <label>Jet personalisé <small style="color:grey; font-size: 10px;">(ignore les options ci-dessus)</small></label>
+            <div class = "input-group">
+              <input id="txt_custom_roll" type = "text" class = "form-control">
+              <span class = "input-group-btn">
+                 <button id="btn_custom_roll" class = "btn btn-default" type = "button">
+                    Roll
+                 </button>
+              </span>
+            </div><!-- /input-group -->
+          </div>
+
           <div class="col-xs-12">
             <label>Résultat</label>
             <textarea style="resize: none;" rows="15" class="form-control" id="resultat"></textarea>
@@ -62,22 +74,92 @@
     <script type="text/javascript">
       $('[name=roll]').click(function (a) {
 
-        d_faces = this.id;
-        d_nb = $('#d_nb').val();
-        d_mod = $('#d_mod').val();
-        roll_nb = $('#roll_nb').val();
+        var i;
+        var d_faces = parseInt(this.id);
+        var d_nb = parseInt($('#d_nb').val());
+        var d_mod = parseInt($('#d_mod').val());
+        var roll_nb = parseInt($('#roll_nb').val());
 
-        result = 0;
-        result_str = "";
+        for(i=0; i<roll_nb; i++)
+          roll(d_nb + "D" + d_faces + (d_mod >= 0 ? "+" : "") + d_mod);
+      });
+
+      $('#btn_custom_roll').click(function () {
+        roll($('#txt_custom_roll').val().toUpperCase());
+      });
+
+      function roll(d_string) {
+
+        var match = /^(\d+)?d(\d+)([+-]\d+)?$/.exec(d_string.toLowerCase());
+
+        if (!match) {
+          throw "Invalid dice notation: " + d_string;
+        }
+
+        var i, tmp1, tmp2, d_nb, d_faces, d_mod;
+
+        console.log(d_string);
+
+        tmp1 = d_string.split('D');
+
+        console.log(tmp1);
+
+        d_nb = tmp1[0];
+
+        if(tmp1[1].split('+').length > 1) {
+          tmp2 = tmp1[1].split('+');
+          d_faces = tmp2[0];
+          d_mod = tmp2[1];
+        } else if(tmp1[1].split('-').length > 1){
+          tmp2 = tmp1[1].split('-');
+          d_faces = tmp2[0];
+          d_mod = -tmp2[1];
+        } else {
+          d_faces = tmp1[1];
+          d_mod = 0;
+        }
+
+
+        var result = 0;
+        var tmp = 0;
+        var tmp_str = "";
 
         for(i=0; i<d_nb; i++) {
           tmp = getRandomInt(1, d_faces);
           result += tmp;
-          result_str += '' + tmp + ' + ';
+          tmp_str += '' + tmp + ' + ';
         }
 
-        $('#resultat').append(result_str.substring(0, result_str.length - 2) + ' = ' + result + '\n');
-      });
+        if(d_mod != 0) {
+          result += parseInt(d_mod);
+          mod_str = (d_mod > 0 ? "plus " : "moins ") + Math.abs(d_mod) + " ";
+          mod_str_cond = d_mod > 0 ? '+'+d_mod : d_mod ;
+        } else {
+          mod_str = "";
+          mod_str_cond = "";
+        }
+
+        if(d_nb > 1) {
+          result_str = tmp_str.substring(0, tmp_str.length - 2) + ' = ' + result;
+        }
+
+        if (d_mod != 0) {
+          result_str = tmp_str.substring(0, tmp_str.length - 2) + (d_mod > 0 ? ('+ '+d_mod) : ('- ' + Math.abs(d_mod)) ) + ' = ' + result;
+        }
+
+        if(d_nb == 1 && d_mod == 0) {
+          result_str = result;
+        }
+
+        $('#resultat').append(
+                "Jet " + ((d_nb > 1) ? ("de " + d_nb + " dés") : ("d'un dé"))
+                + " à " + d_faces + " faces "
+                + mod_str
+                + "(" + d_nb + "D" + d_faces + mod_str_cond + ") "
+                + ":\n");
+
+        $('#resultat').append(result_str + '\n');
+      };
 
       function getRandomInt(min, max) {
           return Math.floor(Math.random() * (max - min + 1)) + min;
